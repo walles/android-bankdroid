a1ab33f6767a13be1d0e14e5b54de5815f49c4e8
Johan Walles <johan.walles@gmail.com>
2016-10-14 22:13:34 +0200

    Add a dummy test to make FindBugs happy

---
+++ bankdroid-core/src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class DummyTest {
+    @Test
+    public void testNothing() {
+        // This test is here to give FindBugs something to analyze.
+        // As soon as the first real test is added to bankdroid-core,
+        // this class can be removed.
+    }
+}
